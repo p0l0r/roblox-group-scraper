440de06fb189be0e066c4bf740d44d1bd43d5b2c
p0l0r <106635045+p0l0r@users.noreply.github.com>
2022-06-21 15:15:23 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import os 
+import threading
+import requests, random
+from dhooks import Webhook
+import ctypes
+ctypes.windll.kernel32.SetConsoleTitleW("Aleks Group Finder")
+
+
+def groupfinder():
+    id = random.randint(1, 1500000)
+    r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}") 
+    if 'owned' not in r.text:
+        re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
+        if 'isLocked' not in re.text and 'owner' in re.text:
+            if re.json()['publicEntryAllowed'] == True and re.json()['owner'] == None:
+                hook.send(f'Hit: https://www.roblox.com/groups/group.aspx?gid={id}')
+                print(f"[+] Hit: {id}")
+            else:
+                print(f"[-] No Entry Allowed: {id}")
+        else:
+            print(f"[-] Group Locked: {id}")
+    else:
+        print(f"[-] Group Already Owned: {id}")
+
+
+print("""
+
+
+____ _    ____ _  _ ____    ____ ____ ____ _  _ ___  
+|__| |    |___ |_/  [__     | __ |__/ |  | |  | |__] 
+|  | |___ |___ | \_ ___]    |__] |  \ |__| |__| |    
+                                                     
+____ _ _  _ ___  ____ ____ 
+|___ | |\ | |  \ |___ |__/ 
+|    | | \| |__/ |___ |  \ 
+                           
+""")
+
+#your webhook
+hook = "https://discord.com/api/webhooks/988871237193310248/ZR_7MIkPzmvQpwfTT0U-q47nl7IZb3K9GT_u1bAnT0DFGAJq-cQI3pixHPRXT8hdR5LX"
+#number of threads
+threads = int("50")
+
+while True:
+    if threading.active_count() <= threads:
+        threading.Thread(target=groupfinder).start()
